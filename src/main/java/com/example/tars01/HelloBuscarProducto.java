5036b390389fee78752ca5c0555f7c9881242c71
Mateo <mateo15rg@gmail.com>
2024-06-01 12:01:49 -0500

    fixed and add classes

---
+++ src/main/java/com/example/tars01/HelloBuscarProducto.java
@@ -0,0 +1,36 @@
+package com.example.tars01;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloBuscarProducto extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        go();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    public static void go() throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("helloBuscarProducto-View.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(),620,350);
+
+        stage.setResizable(false);
+        double screenWidth = Screen.getPrimary().getVisualBounds().getWidth();
+        double screenHeight = Screen.getPrimary().getVisualBounds().getHeight();
+//        stage.setWidth(screenWidth);
+//        stage.setHeight(screenHeight);
+        stage.setScene(scene);
+        stage.show();
+
+
+    }
+}
